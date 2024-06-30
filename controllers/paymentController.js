e3d0b0a6ce6b8285d07ad0331bb447b5bbbba7ae
Emir Çağatay Karaca <emircagataykaraca@gmail.com>
2024-06-30 19:19:55 +0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -151,7 +151,7 @@ router.get("/odeme_basarili", async function (req,res) {
     if (!userS) {
         return res.redirect('/sepet');
     }
-    res.render('order-success', userS)
+    res.render('order-success', {userS})
 })
 
 router.post("/odeme_basarili", async function (req, res) {
